3dad664139d9647b3b60cf5d6d1e2c8c6f077f53
Afonso Menegola <afonsomenegola@gmail.com>
2021-05-16 17:27:13 -0300

    Criando csv com os dados.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-apt-get install openjdk-11-jdk-headless -qq
-wget -q https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
-tar xf spark-3.1.1-bin-hadoop3.2.tgz
-pip install -q findspark
-pip install -q pyspark
-
 # load environemnt variables
 mydir="${0%/*}"
-source "$mydir"environment_variables.sh
+echo "$mydir"
+echo "$( dirname "${BASH_SOURCE[0]}")"
+source "$mydir"/environment_variables.sh
